b9297e57897685c23ea170e0bcb3766099cee185
huangjundashuaige <715817451@qq.com>
2017-11-20 17:31:17 +0800

    extrca

---
+++ game/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1506938217,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/spogebob-sheet0.png",
+		"images/brosqu-sheet0.png",
+		"images/humber-sheet0.png",
+		"images/boom-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
